eb0f020c724d5d6172b73e5feaf57dde1f6519a0
Eng1Mahmoud <mahmoudmohamedabdelaal1@gmail.com>
2023-03-17 23:18:30 +0200

    upgrad model

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
-import express from 'express'
-import * as dotenv from 'dotenv'
-import cors from 'cors'
-import { Configuration, OpenAIApi } from 'openai'
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import { Configuration, OpenAIApi } from "openai";
 
-dotenv.config()
+dotenv.config();
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -11,17 +11,17 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.get('/', async (req, res) => {
+app.get("/", async (req, res) => {
   res.status(200).send({
-    message: 'Hello from AI Chat'
-  })
-})
+    message: "Hello from AI Chat",
+  });
+});
 
-app.post('/', async (req, res) => {
+app.post("/", async (req, res) => {
   try {
     const prompt = req.body.prompt;
 
@@ -29,19 +29,17 @@ app.post('/', async (req, res) => {
       model: "gpt-3.5-turbo",
       messages: [{ role: "user", content: prompt }],
     });
-
-    completion.then((res) =>{
+    completion.then((res) => {
       res.status(200).send({
-        bot:res.data.choices[0].message.content
+        bot: res.data.choices[0].message.content,
       });
-    }
-  );
-   
-
+    });
   } catch (error) {
-    console.error(error)
-    res.status(500).send(error || 'Something went wrong');
+    console.error(error);
+    res.status(500).send(error || "Something went wrong");
   }
-})
+});
 
-app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
+app.listen(5000, () =>
+  console.log("AI server started on http://localhost:5000")
+);
